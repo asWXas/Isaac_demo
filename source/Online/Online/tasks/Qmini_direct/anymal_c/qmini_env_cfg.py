cb08be93bede67cfc1ba0292bc10a952ade83836
asWXas <3635973600@qq.com>
2025-09-30 13:59:02 +0800

    01

---
+++ source/Online/Online/tasks/Qmini_direct/anymal_c/qmini_env_cfg.py
@@ -0,0 +1,11 @@
+import isaaclab.envs.mdp as mdp
+import isaaclab.sim as sim_utils
+from isaaclab.assets import ArticulationCfg
+from isaaclab.envs import DirectRLEnvCfg
+from isaaclab.managers import EventTermCfg as EventTerm
+from isaaclab.managers import SceneEntityCfg
+from isaaclab.scene import InteractiveSceneCfg
+from isaaclab.sensors import ContactSensorCfg, RayCasterCfg, patterns
+from isaaclab.sim import SimulationCfg
+from isaaclab.terrains import TerrainImporterCfg
+from isaaclab.utils import configclass
